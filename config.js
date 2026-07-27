d8d86caa4e74773955218474fb0fca197eb3ffcc
LHCBRE <lindsey.huffman@cbre.com>
2026-07-27 16:33:04 -0700

    Update coordinates in lotConfig

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.lotConfig={p1:{apiId:1,capacity:864,x:13,y:31},p2:{apiId:2,capacity:344,x:41,y:31},p5:{apiId:5,capacity:345,x:11,y:82}};
+window.lotConfig={p1:{apiId:1,capacity:864,x:32,y:75},p2:{apiId:2,capacity:344,x:32,y:25},p5:{apiId:5,capacity:345,x:80,y:82}};
