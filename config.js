8df8d2c92753aceb963c55f760d0b9517b3f16e5
LHCBRE <lindsey.huffman@cbre.com>
2026-07-27 16:49:30 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.lotConfig={p1:{apiId:1,capacity:864,x:28,y:75},p2:{apiId:2,capacity:344,x:28,y:33},p5:{apiId:5,capacity:345,x:83,y:80}};
+window.lotConfig={p1:{apiId:1,capacity:864,x:29,y:75},p2:{apiId:2,capacity:344,x:29,y:28},p5:{apiId:5,capacity:345,x:82,y:82}};
