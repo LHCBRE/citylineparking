7cc4e9232fb6565c1e889c90bf3dbb183ef6a470
LHCBRE <lindsey.huffman@cbre.com>
2026-07-27 16:45:25 -0700

    Modify lotConfig coordinates in config.js
    Updated coordinates for parking lot configurations.

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.lotConfig={p1:{apiId:1,capacity:864,x:32,y:75},p2:{apiId:2,capacity:344,x:32,y:25},p5:{apiId:5,capacity:345,x:80,y:82}};
+window.lotConfig={p1:{apiId:1,capacity:864,x:28,y:75},p2:{apiId:2,capacity:344,x:28,y:33},p5:{apiId:5,capacity:345,x:83,y:80}};
